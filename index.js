5e25994df84650cafed3d1c5104c338c88d5cc36
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-08-20 16:31:29 -0400

    Added a checker for the connection if the connection established call for the prompt init to collect the user input

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 // Imports and requires the inquirer
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 
 // Imports and requires mysql2 npm package
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // Imports and requires console.table npm package
-const consoleTable = require('console.table');
-
+const consoleTable = require("console.table");
 
 // Establishes the connection with the database by using the specified information
 const databaseConnection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '0000',
-    database: 'employee_database'
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "0000",
+  database: "employee_database",
 });
 
+
+// Checks if the database connection failed or gave an error
+// If not logs that connection established and calls for the prompt function 
+databaseConnection.connect(err => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Database connection established");
+  promptInitiate();
+});
+
+// Initialize the prompts and collects the users input 
+const promptInitiate = () => {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      name: "choise",
+      choice: [
+        "View All Employees?",
+        "Add Employee?",
+        "Update Employee Role",
+        "View All Roles?",
+        "Add Role?",
+        "View all Departments",
+        "Add Department?",
+      ],
+    },
+  ]);
+};
